cd48cb0840e73fc3757a580ad9802d16afabbfc6
VaniaVI <vania.vargas.igor@gmail.com>
2025-12-03 19:53:56 -0300

    se le da ecceso al frontend de utilizar todo lo que venga del backend sobre todo lo de la api de ubicaciones

--- usUsuario/src/main/java/com/petsocity/petsocity/config/WebConfig.java
+++ usUsuario/src/main/java/com/petsocity/petsocity/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // tu frontend
+                        .allowedOrigins("http://localhost:3000", "https://petsocity.vercel.app/") // tu frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
